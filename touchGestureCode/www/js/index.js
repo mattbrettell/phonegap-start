6049621cbb065a9dcbf0d61f9ed13bc56f416619
mattbrettell <BREM1_15@uni.worc.ac.uk>
2016-11-17 14:37:31 +0000

    attempt 2

--- touchGestureCode/www/js/index.js
+++ touchGestureCode/www/js/index.js
@@ -2,19 +2,7 @@
 
 $(document).on("pagecreate","#pageone",function(){
   
-  	$('#tapholdtext').on("taphold",function(){
-    	$(this).hide();
- 	});                       
-
-	$('#taptext').on("tap",function(){
-    	$(this).css('color', 'red');
- 	}); 
-
-	$('#swipetext').on("swipeleft",function(){
-    	$(this).css('color', 'green');
-          	});   
-    $('#swipetext').on("swiperight",function(){
-    	$(this).css('color', 'blue');
-    });
-
-});
+  	$('#ontap').on("ontap",function random() {
+	return !Math.round(Math.random());
+}
+);
